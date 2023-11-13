04cddcf1a3035ea28c3a1f0e2296e850389fd3c0
Ricardo Guerra <RikkuG491@gmail.com>
2023-11-13 15:45:18 -0500

    Added solution code

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+// Deliverable 1
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+// Deliverable 2
+const random = Math.round(Math.random() * 10) + 1
+
+// Deliverable 3
+let num3 = 14
+let num4 = 5
+let mod = num3 % num4
+
+// Deliverable 4
+let max = Math.max(2, 5, 8, 15, 20, 7)
