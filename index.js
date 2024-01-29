34da8f152483b7f334f94a15d59e16ff29b19d38
Ricardo Guerra <RikkuG491@gmail.com>
2024-01-29 16:40:11 -0500

    Added solution code

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+// Answer to instruction # 1
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+// Answer to instruction # 2
+const random = Math.ceil((Math.random() * 10) + 1)
+
+// Answer to instruction # 3
+const num3 = 4
+const num4 = 5
+const mod = num3 % num4
+
+// Answer to instruction # 4
+const max = Math.max(1, 2, 3, 20, 4)
